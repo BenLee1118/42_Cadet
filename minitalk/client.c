9899e514ffcb99a5bb228b38ca33d745dc7eb239
Lee Tian Hau <ltian-ha@u82z06s02.42kl.edu.my>
2023-08-21 22:37:01 +0800

    asd

--- minitalk/client.c
+++ minitalk/client.c
@@ -6,23 +6,47 @@
 /*   By: ltian-ha <ltian-ha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/10 22:02:54 by ltian-ha          #+#    #+#             */
-/*   Updated: 2023/08/17 20:59:36 by ltian-ha         ###   ########.fr       */
+/*   Updated: 2023/08/21 22:18:33 by ltian-ha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+void	ft_send_strlen(int pid, int strlen)
+{
+	int	i;
+
+	i = 0;
+	while (i++ < 32)
+	{
+		if (strlen & 0x01)
+		{
+			kill(pid, SIGUSR1);
+		}
+		else
+		{
+			kill(pid, SIGUSR2);
+		}
+		strlen = strlen << 1;
+		usleep(100);
+	}
+}
+
 int	main(int ac, char **av)
 {
 	int pid;
+	int strlen;
 	int	i;
 
 	i = -1;
 	if (ac != 3)
 		return (1);
 	pid = ft_atoi(av[1]);
-	while (av[2][++i])
-	{
+	strlen = ft_strlen(av[2]);
+	ft_send_strlen(pid, strlen);
+	// while (av[2][++i])
+	// {
 		
-	}
+	// }
+	// kill(73987, SIGUSR1);
 }
--- minitalk/server.c
+++ minitalk/server.c
@@ -6,7 +6,7 @@
 /*   By: ltian-ha <ltian-ha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/10 22:02:22 by ltian-ha          #+#    #+#             */
-/*   Updated: 2023/08/17 20:47:38 by ltian-ha         ###   ########.fr       */
+/*   Updated: 2023/08/21 22:26:19 by ltian-ha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 void	ft_receivedata(int signal)
 {
-	if (signal == SIGUSR1)
-	{
-		printf("hi\n\n");
-	}
-	else
-	{
-		printf("bye \n\n");
-	}
+	// if (signal == SIGUSR1)
+	// {
+	// 	printf("%d\n\n", signal);
+	// }
+	// else
+	// {
+	// 	printf("2nd: %d\n\n", signal);
+	// }
 }
 
 int	main(void)
@@ -32,6 +32,7 @@ int	main(void)
 	ft_putnbr_fd(pid, 1);
 	ft_putchar_fd('\n', 1);
 	signal(SIGUSR1, ft_receivedata);
+	signal(SIGUSR2, ft_receivedata);
 
 	while (1)
 	{
